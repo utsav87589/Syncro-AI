33dd12a47dbeb14846c41b0d6b5dca22478af7fc
utsav87589 <gkundukundu987@gmail.com>
2026-07-11 23:19:03 -0400

    feat: scaffold architecture for FetchData container and CodeInsight presentation components

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import CodeInsight from "./apps/code-insight-fetcher/CodeInsight";
+
+export default function app(){
+    return(<>
+        <CodeInsight/>
+    </>);
+}
---
+++ src/apps/code-insight-fetcher/CodeInsight.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+
+export default function CodeInsight(){
+
+    return(<>
+        <h1>Code Insight App</h1>
+    </>);
+
+}
