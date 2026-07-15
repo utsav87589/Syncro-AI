ddb36fe28dde91e67e72ad85709ee0a605c29df6
utsav87589 <gkundukundu987@gmail.com>
2026-07-14 21:34:48 -0400

    style(CodeInsightFetcher): redesign to clean light theme & resolve eslint warning

--- src/apps/CodeInsightFetcher/CodeInsightFetcher.jsx
+++ src/apps/CodeInsightFetcher/CodeInsightFetcher.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import useFetchInsight from "./useFetchInsight";
 import "./CodeInsightFetcher.css";
 
